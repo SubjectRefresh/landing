3bc6ab72b3b117831d90d022388a532d43168b4f
Miles Budden <miles@budden.net>
2015-11-08 18:13:22 +0000

    Added scroll spy

--- js/bin/init.js
+++ js/bin/init.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
     $('.parallax').parallax();
-    $("#index-banner").find(".parallax").css("top","0");
-    if ($.browser.mobile){
-    	$(".parallax-container").css("height","0");
-    	$("#index-banner").css("height","auto").css("padding-bottom","50px").css("border-bottom","2px solid #3f51b5").find(".section").css("background","#fff").css("padding-top","0").parent().find(".parallax").css("top","initial").find("img").css("height","0");
+    $("#index-banner").find(".parallax").css("top", "0");
+    if ($.browser.mobile) {
+        $(".parallax-container").css("height", "0");
+        $("#index-banner").css("height", "auto").css("padding-bottom", "50px").css("border-bottom", "2px solid #3f51b5").find(".section").css("background", "#fff").css("padding-top", "0").parent().find(".parallax").css("top", "initial").find("img").css("height", "0");
     }
+});
+$(document).ready(function () {
+    $('.scrollspy').scrollSpy();
 });
